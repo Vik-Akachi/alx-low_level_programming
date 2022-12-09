dfdd12bf31ca17aaf4aed2794326f1d8cd2953c0
Vik-Akachi <victorakachi89@gmail.com>
2022-12-08 17:19:36 -0800

    4446464

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,12 @@
 /**
  * main - Entry point
  *
- * Description: prints "programming is like building a muiltilingual puzle"
+ * Description: prints "programming is like building a muiltilingual puzzle"
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
