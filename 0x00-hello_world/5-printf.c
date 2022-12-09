57d89f1df195a787ecb0f1a9b79f7265e7fa0ba6
Vik-Akachi <victorakachi89@gmail.com>
2022-12-08 17:03:11 -0800

    555

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * mani - Entry point
  *
- * Description: prints “with proper grammar, the outcome is a piece of art”
+ * Description: prints "with proper grammar, the outcome is a piece of art".
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf(“with proper grammar, but the outcome is a piece of art, \n”);
+	printf("with proper grammar, but the outcome is piece of art, \n");
 	return (0);
 }
