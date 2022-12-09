174d69c0dfcbaaf3d0930a486f6b0b2698a9401b
Vik-Akachi <victorakachi89@gmail.com>
2022-12-08 18:21:32 -0800

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - print quote
+ *
+ * Description: prints Dora's quote
+ *
+ * Return: Always1 (Success)
+ */
+int main(void)
+{
+	write(2,"and that piece od art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
