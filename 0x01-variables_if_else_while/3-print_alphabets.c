f2fa2da188ffc650d3d597c706a5abdb210c4828
Vik-Akachi <victorakachi89@gmail.com>
2022-12-09 14:50:03 -0800

    kkdj

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 /**
- * main -hi betty 
+ * main -hi betty
  *
  * Return: always 0 (Success)
  */
 int main(void)
 {
-	int i, j;
+	int b, c;
 
-	i = 97;
-	j = 65;
-
-while
+	b = 97;
+	c = 65;
+	while (b < 123)
+	{
+		putchar(b);
+		b++
+	}
+	while (c < 91)
+	{
+		putchar(c);
+		c++
+	}
+	putchar(10);
+	return (0);
 }
