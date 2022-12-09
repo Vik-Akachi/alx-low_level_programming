122bb70840363f47036fe1c6621d995483d96c52
Vik-Akachi <victorakachi89@gmail.com>
2022-12-08 16:21:12 -0800

    4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+ * main - Main Entry point.
+ *
+ * Description:"with proper grammar, but the outcome is a piece of art,"
+ *  
+ * Description - main always entry point.
+ * 
+* return 0 (Success).
+*/
+int main(void)
+/*main -main entry point*/
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
