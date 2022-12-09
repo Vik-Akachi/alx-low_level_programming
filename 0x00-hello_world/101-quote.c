095296b066ea2c37411ec9b79a83e94fbf7c471d
Vik-Akachi <victorakachi89@gmail.com>
2022-12-08 18:30:03 -0800

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,12 +4,12 @@
 /**
  * main - Print quote
  *
- * Description: Prints Dora's quote
+ * Description: irints Dora's quote
  *
  * Return: Always1 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece od art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
