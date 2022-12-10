a9919e796f41c7260b1ff8a1789a66beb1d50032
Vik-Akachi <victorakachi89@gmail.com>
2022-12-09 17:22:35 -0800

    87

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+/**
+ * main - madam am here today
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
 
+{
+	int a = "a";
+	int a = < "z";
+	int a++, \n;
+
+		puchar(a, \n);
+
+	return (0);
+}
