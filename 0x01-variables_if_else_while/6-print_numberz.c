a4bb349703c4b2e1430349e9801e077c761f3173
Vik-Akachi <victorakachi89@gmail.com>
2022-12-09 18:40:22 -0800

    uue

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - am coming for you
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 56)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
