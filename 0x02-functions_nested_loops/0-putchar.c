afa55a44baaf4b6258f7d5d90fb1e3e006fb6e7b
Vik-Akachi <victorakachi89@gmail.com>
2022-12-13 05:11:43 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,12 @@
  * Return: always 0
  */
 
-int main(void)
+int _putchar(void)
 {
 	char(95, p, u, t, c, h, a, r)
 
-	putchar(%c %c %c %c %c %c %c %c, 95, p, u, t, c, h, a, r);
+	putchar("%c" "%c" "%c" "%c" "%c" "%c" "%c" "%c", 95, p, u, t, c, h, a, r);
 
 	_putchar("\n");
-
 	return (0);
 }
-
-
