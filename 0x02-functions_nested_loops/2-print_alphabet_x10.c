41d6b3455805233b5d3472a3d1433fd970adee89
Vik-Akachi <victorakachi89@gmail.com>
2022-12-13 10:11:06 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,11 +14,10 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 
 	{
-		for (j = 97; j < = 122; j++)
-		_putchar("%c", j);
+		for (j = 'a'; j <= 'z'; j++)
 		{
-			_putchar('\n);
-
+			_putchar(j);
 		}
+		_putchar('\n');
 	}
 }
