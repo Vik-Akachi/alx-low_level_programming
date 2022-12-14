090ba4bc837facb376460498c3c35ff2ef918d5b
Vik-Akachi <victorakachi89@gmail.com>
2022-12-14 15:30:46 -0800

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *main - a code that tests that this function is positive_or_negative() 
+ * and gives the correct output when given a case of 0.
+ * @n: the integer to be tested
+ * return: positive or negative
+ */
+
+void positive_or_negative(int i)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf(positive_number "%d\n", n);
+	}
+	else
+	{
+		printf(negative_number "%d\n", n);
+	}
+	return (0);
+}
