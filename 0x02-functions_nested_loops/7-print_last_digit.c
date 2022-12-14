5a32110b5061100660010e75f5050184649e1bc9
Vik-Akachi <victorakachi89@gmail.com>
2022-12-14 06:42:04 -0800

    cor

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_last_digit -> prints the last digit
- * @n: the passed args
- * Return: the last digit
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number in question.
+ *
+ * Return: Value of the last digit.
  */
 int print_last_digit(int n)
 {
+	int last_digit = n % 10;
 
-	int x;
+	if (last_digit < 0)
+		last_digit *= -1;
 
-	if  (n < 0)
-		n = -n;
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+	_putchar(last_digit + '0');
 
+	return (last_digit);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,23 @@
 #include "main.h"
+
 /**
- * jack_bauer - print every minute of the day of jack Bauer
- *
- * Return: ...
+ * jack_bauer - Prints every minute of the day of
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
 void jack_bauer(void)
 {
-	int i, j, k, m;
+	int hour, minute;
 
-	for (i = 0; i <= 2; i++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
-			{
-				for (k = 0; k <= 5; k++)
-				{
-					for (m = 0; m <= 9; m++)
-					{
-						_putchar(i + '0');
-						_putchar(j + '0');
-						_putchar(58);
-						_putchar(k + '0');
-						_putchar(m + '0');
-						_putchar('\n');
-					}
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
