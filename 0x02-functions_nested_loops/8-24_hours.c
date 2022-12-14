b4284bad8b94ddafc9a6d35dce028100515bdaf6
Vik-Akachi <victorakachi89@gmail.com>
2022-12-13 21:18:05 -0800

    g

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,43 +1,32 @@
 #include "main.h"
-
 /**
- * jack_bauer -> prints 24 hours
+ * jack_bauer - print every minute of the day of jack Bauer
  *
+ * Return: ...
  */
-
 void jack_bauer(void)
 {
-	int i, j;
+	int i, j, k, m;
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i <= 2; i++)
 	{
-		for (j = 0;  j < 60; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if (i < 10)
+			if ((i <= 1 && j <= 9) || (i <= 2 && j <= 3))
 			{
-				_putchar('0');
-				_putchar(i + '0');
+				for (k = 0; k <= 5; k++)
+				{
+					for (m = 0; m <= 9; m++)
+					{
+						_putchar(i + '0');
+						_putchar(j + '0');
+						_putchar(58);
+						_putchar(k + '0');
+						_putchar(m + '0');
+						_putchar('\n');
+					}
+				}
 			}
-				else if (i > 10)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			if (j < 10)
-			{
-				_putchar(':');
-				_putchar(0);
-				_putchar(j + '0');
-			}
-				else if (j >= 10)
-			{
-				_putchar(':');
-				_putchar((j / 10) + 0);
-				_putchar((j % 10) + '0');
-
-			}
-				_putchar('\n');
 		}
-
 	}
 }
