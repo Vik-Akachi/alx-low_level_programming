867bd924a81ac7c8776ac179ab5ae937f129d5ed
Vik-Akachi <victorakachi89@gmail.com>
2022-12-14 13:14:46 -0800

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,27 +5,27 @@
  * @a: fir integer
  * @b: second integer
  * @c: third integer
+ * Return: largest number
  */
 
 int largest_number(int a, int b, int c)
 {
+
 	int largest;
 
-	for (a == 972; b == -98; c == 0)
+	if (a < b && b < c)
 	{
-		if (a < b && b < c)
-		{
-			printf (largest = "%d",  a);
-
-		else if (b > a && a > c)
-		{
-			printf (largest = "%d",  b);
-		}
-
-		else
-		{
-			printf (largest = "%d", c);
-		}
+		largest =  a;
 	}
+	else if (b > a && a > c)
+	{
+		largest =  b;
+	}
+
+	else
+	{
+		largest = c;
+	}
+
 	return (largest);
 }
