cda1e5f3d65d85b86dbc3e4a35209d058de822cf
Vik-Akachi <victorakachi89@gmail.com>
2022-12-13 18:52:53 -0800

    dd

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,16 +8,16 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (1 0; i < 24, i++)
+	for (1 = 0; i < 24; i++)
 	{
 		for (j = 0;  j < 60; j++)
 		{
 			if (i < 10)
 			{
-				_putcha('0');
-				_putchar(i + '0')
+				_putchar('0');
+				_putchar(i + '0');
 			}
-			else if (i > 10)
+				else if (i > 10)
 			{
 				_putchar((i / 10) + '0');
 				_putchar(i % 10) + '0');
