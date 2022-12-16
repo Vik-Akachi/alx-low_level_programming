0edfd8efec43ff304f0ff886be8bcb4cde800260
Vik-Akachi <victorakachi89@gmail.com>
2022-12-16 06:39:01 -0800

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,11 +12,14 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 10; a++)
-		_putchar (a + '0');
+	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b <= 14; b++)
-			_putchar (b + '0');
+		for (b = 0; b <= 9; b++)
+		{
+			if (b >= 10)
+				_putchar ((b / 10) + '0');
+			_putchar ((b % 10) + '0');
+		}
+		_putchar ('\n');
 	}
-	_putchar ('\n');
 }
