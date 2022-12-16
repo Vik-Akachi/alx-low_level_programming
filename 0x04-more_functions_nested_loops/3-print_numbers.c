20e173eb33e727e13097ffb9f6a0a737be48d617
Vik-Akachi <victorakachi89@gmail.com>
2022-12-16 03:45:18 -0800

    1

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers -> Writes a function that prints the numbers, from 0 to 9
  *
- * @ putchar: returns no argument
+ * @ _putchar: returns no argument
  *
  * return: always 0
  */
@@ -13,6 +13,6 @@ void print_numbers(void)
 	int a;
 
 	for (a = 0; a <= 9; a++)
-		putchar (a + '0');
-	putchar ('\n');		
+		_putchar (a + '0');
+	_putchar ('\n');		
 }
