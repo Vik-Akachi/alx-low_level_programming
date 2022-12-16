733bcdf69ffaec61c25abdfe2f6abecaa7d07272
Vik-Akachi <victorakachi89@gmail.com>
2022-12-16 15:29:42 -0800

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,17 +12,25 @@
 
 void print_triangle(int size)
 {
-	int a, b;
+	int row, hashes, spaces;
 
-	for (size <= 0)
-		_putchar ('\n');
-
-	for (a = 0; a <= size; a++)
+	if (size <= 0)
 	{
-		if (b = 0; b <= a; b--)
-		{
-			_putchar ("35");
-		}
 		_putchar ('\n');
 	}
-
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar (' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+		}
+	}
+}
