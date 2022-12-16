55090a81926b97edc1f24b8fc3aae728b3c72e1c
Vik-Akachi <victorakachi89@gmail.com>
2022-12-16 05:01:01 -0800

    m

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * mul - function that multiplies two numbers
+ * mul - multiplies two numbers
  *
- * @a: integer 1
- * @b: interger 2
+ * @a: integer  param1
+ * @b: interger param2
  *
- * return: always 0
+ * Return: success and interger
  *
  */
 
