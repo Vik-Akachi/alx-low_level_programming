39df99cb82fd36bde782b48e509d187b264cbabe
Vik-Akachi <victorakachi89@gmail.com>
2022-12-16 05:52:52 -0800

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,9 +12,10 @@ void print_most_numbers(void)
 {
 	int a;
 
-	for ((a = 0; a < 10) && (a != 2; a != 4))
+	for (a = 0; a < 10;)
 	{
-		_putchar (a = '0');
+		if (a != 2 && a != 4)
+			_putchar (a + '0');
 	}
 	_putchar ('\n');
 }
