1c91aee40bc6cf12851515b9a8be556be5367f7e
Vik-Akachi <victorakachi89@gmail.com>
2022-12-16 16:42:08 -0800

    3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,15 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
-	else
+	else 
 	{
 		largest = c;
 	}
