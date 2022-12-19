b7282aff2e5d9a0382eeb652b9e5f3c57c730086
Vik-Akachi <victorakachi89@gmail.com>
2022-12-19 13:09:20 -0800

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the value from string a to string b
+ *
+ * @src: destination 1
+ * 
+ * @dest: desination 2
+ *
+ * Return:always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	if (a = 0; a < src[a]; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
