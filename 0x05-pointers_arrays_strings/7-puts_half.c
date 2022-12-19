b0c57df8b00827fcf2ca1bef652b9d4d97fd565c
Vik-Akachi <victorakachi89@gmail.com>
2022-12-19 12:30:17 -0800

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - a function that prints half of a string
+ *
+ * _putchar: character diplay function
+ *
+ * Return: always 0
+ */
+
+void puts_half(char *str)
+{
+	int color = 0, n;
+	
+	while (str[color] != '\0')
+		color++;
+	if (color + 1 % 2 != '0')
+		n = (color - 1) / 2;
+	else
+		n = (color /2);
+	n++;
+
+	for (color =n; str[color] != '\0'; color++)
+	_putchar (str[color]);
+	_putchar ('\n');
+}
