291f774131fa3bba121979b9a35be09f1350882b
Vik-Akachi <victorakachi89@gmail.com>
2022-12-19 06:49:14 -0800

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - for a function that swaps two integers
+ *
+ * @a: fir integer
+ * @b: second interger
+ *
+ * Return: always 0
+ */
+
+void swap_int(int *a, int *b)
+
+{
+	*a == *b && *b == *a
+}
