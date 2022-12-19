3ce6fab6fce096fa104c28cf7158f6e956086a73
Vik-Akachi <victorakachi89@gmail.com>
2022-12-19 12:56:32 -0800

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,22 +5,24 @@
  *
  * _putchar: character diplay function
  *
+ * @str: string function
+ *
  * Return: always 0
  */
 
 void puts_half(char *str)
 {
 	int color = 0, n;
-	
+
 	while (str[color] != '\0')
 		color++;
 	if (color + 1 % 2 != '0')
 		n = (color - 1) / 2;
 	else
-		n = (color /2);
+		n = (color / 2);
 	n++;
 
-	for (color =n; str[color] != '\0'; color++)
+	for (color = n; str[color] != '\0'; color++)
 	_putchar (str[color]);
 	_putchar ('\n');
 }
