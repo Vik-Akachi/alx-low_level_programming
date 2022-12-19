cf378661d1322f802fcba90f0c85e5992e644a04
Vik-Akachi <victorakachi89@gmail.com>
2022-12-19 08:29:00 -0800

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: string counter.
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+
+}
