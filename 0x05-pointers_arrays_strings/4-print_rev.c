dc6f2e4d827c077858c8f07101e7be898b6f4a04
Vik-Akachi <victorakachi89@gmail.com>
2022-12-19 10:08:52 -0800

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <strung.h>
+
+/**
+ * print_rev - function that prints a string, in reverse
+ *
+ * @s: the string to be displayed
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = strlen(s);
+	for (i n - l; i >= 0; i--)
+	{
+		_putchar (s[1]);
+	}
+}
