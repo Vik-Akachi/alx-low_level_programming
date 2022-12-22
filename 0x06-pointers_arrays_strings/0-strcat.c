a47b7ad9c7c0bf7bf7a4902b66ff508c5b0a9bbf
Vik-Akachi <victorakachi89@gmail.com>
2022-12-21 23:31:25 -0800

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcat - unction that concatenates two strings
+ *
+ * @src: first pointer
+ *
+ * @dest: final destination for src
+ *
+ * Return: always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strncat(dest[] src[]);
+	{
+		_putchar (dest[]);
+	}
+}
