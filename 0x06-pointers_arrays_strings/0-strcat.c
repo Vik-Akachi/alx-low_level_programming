dba0b7a81918099b4158217905df0a50a674d998
Vik-Akachi <victorakachi89@gmail.com>
2022-12-22 00:14:31 -0800

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,13 +7,26 @@
  *
  * @dest: final destination for src
  *
+ * @i: string length
+ *
+ * @n: new destination
+ *
  * Return: always 0
  */
 
 char *_strcat(char *dest, char *src)
 {
-	strncat(dest[] src[]);
-	{
-		_putchar (dest[]);
-	}
+	int dest_length = strlen(dest);
+	int src_length = strlen(src);
+	int size = dest_length + drc_length +1;
+	char *n = calloc(size, sizeof(char));
+
+	for (int i = 0; i < dest_length; i++)
+		n[i] = dest[i];
+	for (int i = 0; i < src_length; i++)
+		n[dest_length +i] = src[i];
+
+	n[size -1] = '\0';
+
+	_putchar ('n');
 }
