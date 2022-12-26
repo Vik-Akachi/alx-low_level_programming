150e545766535c62066c51889b441f858e603706
Vik-Akachi <victorakachi89@gmail.com>
2022-12-26 12:50:13 -0800

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/*
+ * _strncpy - point of entry
+ *
+ * @dest: final destination
+ *
+ * @src: to be copied
+ *
+ * @n: string length
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+	int s1;
+
+	for (s1 = 0; s1 < n && *(src + s1); s1++)
+	{
+		*(dest + s1) = *(src + s1);
+	}
+	for (; s1 < n; s1++)
+	{
+		*(dest + s1) = '\0';
+	}
+
+	return (dest);
+}
