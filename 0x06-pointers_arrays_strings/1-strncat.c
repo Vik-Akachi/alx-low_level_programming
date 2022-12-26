167fe1011d3ff44cb920b848768829c6ac8e5fc6
Vik-Akachi <victorakachi89@gmail.com>
2022-12-26 12:20:22 -0800

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,24 +17,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int size1 = 0;
-	int size2 = 0;
+	int size1;
+	int size2;
 
-	while (*(dest + size1) != '\0')
+	for (size1 = 0; dest[size1] != '\0'; size1++)
 	{
-		dest++;
+		continue;
 	}
 
-	while (size2 < n)
+	for (size2 = 0; src[size2] != '\0' && size2 < n; size2++)
 
 	{
-		*(dest + size1) = *(src + size2);
-		if (*(src + size2) == '\0')
-			break;
-
-		size1++;
-		size2++;
+		dest[size1 + size2] = src[size2];
 	}
+	dest[size1 + size2] = '\0';
 
 	return (dest);
 }
