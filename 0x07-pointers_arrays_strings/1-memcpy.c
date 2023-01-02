53bc8dc557e5ce7e02c993c86e062eb9490a8949
Vik-Akachi <victorakachi89@gmail.com>
2023-01-02 09:45:37 -0800

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy -  a function that copies one memory to the other
+ *
+ * @dest: final destination of n
+ * @src: initial destination of n
+ * @n: maximum size of memory to be copied
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+	int b;
+
+	for (a = 0; a <= n; a++)
+
+	{
+		a = src[n];
+		dest[n] = a;
+	}
+	return (dest);
+}
