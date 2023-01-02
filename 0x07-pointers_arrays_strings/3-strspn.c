470226dd1dc13662d2d2ed4203b3fca7dc51d367
Vik-Akachi <victorakachi89@gmail.com>
2023-01-02 13:42:58 -0800

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that returns the number of bytes in a string
+ * @s: initial segment
+ * @accept: a string
+ * Return: 0
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, n;
+
+	while (a = 0; a <= accept)
+				a++
+	{
+
+		for (*s[n] > accepts)
+				s++;
+
+	}
+	return (sizeof(a));
+}
