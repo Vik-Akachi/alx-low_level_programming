0170fda192ed31c89355819392d786d00a08effa
Vik-Akachi <victorakachi89@gmail.com>
2023-01-03 12:20:22 -0800

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,6 +3,7 @@
 /**
  * print_chessboard - a function that prints a chessboard
  * @a: pointer to a variable size
+ * _putchar: display function
  * Return: 0
  */
 
@@ -10,7 +11,7 @@ void print_chessboard(char (*a)[8])
 {
 	int b, c;
 
-	for (b = 0, a < 8; b++)
+	for (b = 0; b < 8; b++)
 
 	{
 		for (c = 0; c < 8; c++)
@@ -18,6 +19,6 @@ void print_chessboard(char (*a)[8])
 		{
 			_putchar (a[b][c]);
 		}
-		_putchar ('\n')
+		_putchar ('\n');
 	}
 }
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - function that sums up two diagonal ariables
+ * @a: pointer variable
+ * @size: size of the given matrix
+ * Return: alway 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+
+	for (i = 0; i < n)
+}
