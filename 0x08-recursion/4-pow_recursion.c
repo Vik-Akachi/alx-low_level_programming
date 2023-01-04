265d439c1ebd79c15f41738947e2b3eb6baac2a7
Vik-Akachi <victorakachi89@gmail.com>
2023-01-04 13:20:41 -0800

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,7 +16,7 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	}
 	else if (n <= y)
-		return (x * _pow_recursion(x( n + 1));
+		return (x * _pow_recursion(x * ( n + 1));
 	else
 	{
 		return (1);
