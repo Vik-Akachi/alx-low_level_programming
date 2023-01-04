27d48246655b51b08e3b75f0308f42e200c9e38a
Vik-Akachi <victorakachi89@gmail.com>
2023-01-04 10:03:29 -0800

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,14 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_print_rev_recursion(s - 1);
-				s--;
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (*s)
+{
+_putchar(*s);
+_print_rev_recursion(s - 1);
+s--;
+}
+else
+{
+_putchar('\n');
+}
 }
