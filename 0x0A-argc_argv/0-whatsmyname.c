8165d222a8c3d1df66292983c83e1adae974aa07
Vik-Akachi <victorakachi89@gmail.com>
2023-01-06 15:16:30 -0800

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * -whatsmyname - argument function that prints name of address
+ *  @argc: argument count
+ *  @argv: argiment address in vector array
+ *  Return: 0
+ */
+
+int _whatsmyname(int argc, char* argv[])
+{
+	printf ("argc = %d\n", argc);
+	return (0);
+}
