ac06274b8e10b92526493e7695c802c4bdbf2751
Vik-Akachi <victorakachi89@gmail.com>
2023-01-06 15:26:50 -0800

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,10 @@
  *  Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
 	(void)argc;
 
-	printf ("argc = %s\n", argv[0]);
+	printf ("%s\n", argv[0]);
 	return (0);
 }
