8d85f2f6969f154a9852a398a3ed1e5327f3302f
Vik-Akachi <victorakachi89@gmail.com>
2023-01-06 16:24:05 -0800

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argiment address in vector array
+ * @a: argument presented
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a
+		for (a > 0; a < argc; a++)
+		{
+			printf("%d\n", argv[a]);
+			return (0);
+		}
+}
