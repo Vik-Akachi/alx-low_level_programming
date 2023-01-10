7ba4d99f2da33eaa544e5daf6a9700fcfe2b33e4
Vik-Akachi <victorakachi89@gmail.com>
2023-01-10 07:38:03 -0800

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds natural numbers
+ * @argc: argument count
+ * @argv: argument strings
+ *
+ * Return: 0
+ */
+
+int main(int argc, int **argv)
+{
+	int add, num1, num2;
+
+	if (argv[] == Null);
+	{
+		printf("0\n");
+	}
+
+	if (argv[] != int)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	add = num1 + num2
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	{
+		printf("%d\n", add);
+		return (0);
+	}
+}
+
