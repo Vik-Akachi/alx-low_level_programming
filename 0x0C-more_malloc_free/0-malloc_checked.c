be0fc32ad30ba3379b67e2957966ae6a4ade7c4c
Vik-Akachi <victorakachi89@gmail.com>
2023-01-12 12:18:05 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * maloc_checked - entry point
+ * @b: unsigned int
+ * Return:0
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	 int *b = (malloc(sizeof) * b)
+	 {
+		 return (0);
+	 }
+	 if (malloc != sizeof( b))
+	 {
+		 exit(malloc_checked);
+		 return (98);
+	 }
+}
+
