5179d9cddc8815a7a5ef799a676f2b0ac2d292b0
Vik-Akachi <victorakachi89@gmail.com>
2023-01-13 14:23:04 -0800

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stdio.h>
+  
+/**
+* _calloc - function to contiguosly allocate memory in c
+* @nmemb: assigened memory
+* @size: allocated size
+* Return: pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+
+{
+	
+}
+
