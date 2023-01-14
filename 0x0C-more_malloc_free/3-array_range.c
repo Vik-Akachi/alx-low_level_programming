cf09ae38c5668a033525c1786d07fa8ae9bdbaa4
Vik-Akachi <victorakachi89@gmail.com>
2023-01-13 22:26:55 -0800

    3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * array_range - allocates and prints a range of arrays
+ * @min: smallest number
+ * @max: largest number
+ * @i: pointer
+ * @a: varriables
+ * Return: i
+ */
+
+int *array_range(int min, int max)
+{
+	char *i;
+	int a;
+	
+	for (a = 0; a < i[i - 1]; a++)
+	{
+		for (a = min; a < max; a++)
+	}
+
+	*i = malloc(sizeof(int) * a);
+
+	if (a > (a -1))
+	{
+		return (NULL);
+	}
+	else if (i == 0)
+	{
+		return (NULL);
+	}
+	else
+		return (i);
+}
