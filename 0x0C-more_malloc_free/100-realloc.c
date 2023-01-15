c0f8b17cbd4c85208365f3fd1e16691152fe1fd5
Vik-Akachi <victorakachi89@gmail.com>
2023-01-15 08:59:42 -0800

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - a function that reallocates function
+ * @ptr: pointer address
+ * @old_size: malloc'ed size
+ * @new_size: realloc'ed size
+ * Return: ptr
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	
+}
