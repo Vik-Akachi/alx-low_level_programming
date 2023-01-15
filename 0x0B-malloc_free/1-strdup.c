829bcbac7969e70485c7cd2e28bf67bc635697e1
Vik-Akachi <victorakachi89@gmail.com>
2023-01-15 14:50:07 -0800

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - string duplicator
+ * @str: initial string
+ * Return: str
+ */
+
+char *_strdup(char *str)
+{
+	char *str1;
+
+	int i, count = 0;
+
+	for (str[] == str1[])
+	{
+	if (str1 == NULL)
+		return (NULL);
+
+	str1 = malloc(sizeof(i) * count);
+
+	if (count == 0)
+		return (NULL);
+
+	for (i = 0; i < count; i++)
+		str1[i] = i + 1;
+
+	return (str1);
+	}
+}
