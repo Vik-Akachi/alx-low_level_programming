d7929d90b355ef3322298e2955ef2b82d490f7ed
Vik-Akachi <victorakachi89@gmail.com>
2023-01-17 05:32:54 -0800

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,26 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dog details
+ * @name: dog's identity
+ * @age: dog's age
+ * @owner: dogs owner
+ */
+
 struct dog
 {
-	char *name[10];
-	float *age[3];
-	char *owner[15];
-}
-int maon(void)
-{
-	struct dog dog;
+	char *name;
+	float age;
+	char *owner;
+};
 
-	return (0);
-}
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d)
+
+#endif
