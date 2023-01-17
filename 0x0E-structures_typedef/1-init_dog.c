e24137804f7b83b75cbe966fb7564c3eb2247600
Vik-Akachi <victorakachi89@gmail.com>
2023-01-17 06:11:34 -0800

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * struct dog *d -> place holder for the dog struct
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	return (dog);
+}
