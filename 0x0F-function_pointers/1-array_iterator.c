f88df4fefe6535b8ffe3687c02c3fcd4f932dc68
Vik-Akachi <victorakachi89@gmail.com>
2023-01-19 00:56:47 -0800

    1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,8 @@
  * array_iterator - prints all the members of an array
  * @array: elements in the array
  * @size_t: size  allocation to the array
- *
+ * @action: pointer to the action on the array
+ * @size: size of the array
  * Reyurn: 0
  */
 
@@ -15,8 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	for (i = 0; i > size; i++)
 		;
-	ptr_action = malloc((size_t) size);
-	
+	ptr_action = malloc((size_t) size);	
 
 	return (0);
 }
