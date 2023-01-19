a224d5079a2c5a6f828d91cdcc9432948363c82e
Vik-Akachi <victorakachi89@gmail.com>
2023-01-18 23:05:08 -0800

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function-pointers.h"
+
+/**
+ * array_iterator - prints all the members of an array
+ * @array: elements in the array
+ * @size_t: size  allocation to the array
+ *
+ * Reyurn: 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+
+	return (0);
+}
