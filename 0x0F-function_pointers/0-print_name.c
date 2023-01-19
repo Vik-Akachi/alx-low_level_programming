2b9c966777867eb55c6431bb5a1177c409d37eab
Vik-Akachi <victorakachi89@gmail.com>
2023-01-18 21:09:12 -0800

    0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * print_name - prints the operator's name
+ * @name: name of user
+ * @f: name in capital letters
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	int i, count;
+	for (i = 0; i < count; i++)
+		for (count = o; count < name; count++)
+
+	_putchar ((char *) "name %\n", name);
+
+}
+
+int main(void)
+{
+	void *malloc(size_t *name);
+
+	void (*name_ptr)(char) = &name;
+	(*name_ptr)(*f);
+	retur (0);
+}
