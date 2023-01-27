d141bbbb6a5597009d8e664bbf62bdcd327fa0fb
Vik-Akachi <victorakachi89@gmail.com>
2023-01-27 13:09:54 -0800

    2

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * list_len -> displays the count of the string characters
+ * @h: pointer to the address of string
+ *
+ * Return: somthing
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count;
+	int i;
+
+	list_len = i;
+
+
+	while (h)
+	{
+		for  (i = 0; i < count; i++)
+		{
+			printf("%d\n", i);
+		}
+		count++;
+	}
+	return (0);
+}
+
