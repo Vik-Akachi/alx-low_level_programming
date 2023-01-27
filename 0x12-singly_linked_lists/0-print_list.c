bd412bcb29c44b4c4f0066a63dde8f8cd654860f
Vik-Akachi <victorakachi89@gmail.com>
2023-01-27 12:31:41 -0800

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,30 +1,30 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 
 /**
  * print_list -> prints the given list of data
- * @size_t: memory size allocated to the pointer
- *
- * Return: something 
+ * @h: pointer to the memory
+ * Return: something
  */
 
 
 size_t print_list(const list_t *h)
 {
 	int count = 0;
-	
+
 	while (h)
 	{
 		if (h->str == NULL)
 		{
-			print("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", h->len, h->str);
 		}
 		count++;
 		h = h->next;
