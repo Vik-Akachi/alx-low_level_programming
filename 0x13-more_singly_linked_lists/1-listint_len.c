a2f1496bfe860839df555b6ab5b210e147320a30
Vik-Akachi <victorakachi89@gmail.com>
2023-01-30 11:11:34 -0800

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - returns the number of elements in a list
+ * @h: holder of thge address of the firsty node
+ *
+ * Return: number
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		{
+			printf("%d\n", count);
+		}
+		count++;
+	}
+	return (count);
+}
