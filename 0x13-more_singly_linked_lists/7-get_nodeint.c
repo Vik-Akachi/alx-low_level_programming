58739d26927a5ed3fa949c2adbe18ab1e5a9a4d5
Vik-Akachi <victorakachi89@gmail.com>
2023-02-01 09:17:19 -0800

    1

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - returns the index of the last node
+ * @head: stores the address of the first node
+ * @index: the number in succession of the last node
+ *
+ * Return: n
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int n;
+
+	if (head == NULL)
+		return (NULL);
+	for (n = 0; n <= index; n++)
+	{
+		head = head->next;
+		{
+			if (head == NULL)
+				return (NULL);
+		}
+	}
+	return (head);
+}
