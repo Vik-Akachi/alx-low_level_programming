3e1e148ca7b80ff61093e15f14cc3f149841c243
Vik-Akachi <victorakachi89@gmail.com>
2023-02-01 09:21:03 -0800

    1

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,13 +16,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	for (n = 0; n <= index; n++)
+	for (n = 0; n < index; n++)
 	{
 		head = head->next;
-		{
-			if (head == NULL)
-				return (NULL);
-		}
+		if (head == NULL)
+			return (NULL);
 	}
 	return (head);
 }
