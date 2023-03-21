c0d7666266f509f14037cbd7ce5776f7b6efb850
Vik-Akachi <victorakachi89@gmail.com>
2023-03-21 13:34:36 -0700

    1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
